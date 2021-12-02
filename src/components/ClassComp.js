71d94ec1606db5201c4f079335209d93885040ce
edwardBenedict <edwardbenedict92@gmail.com>
2021-12-02 22:45:36 +0300

    last commit

--- src/components/ClassComp.js
+++ src/components/ClassComp.js
@@ -1,25 +1,27 @@
 import React from "react";
-
 export default class ClassComp extends React.Component {
   constructor(props) {
     super(props);
     this.state = { student: "Yusuf", counter: 0 };
   }
-
-  changeCounter = () => {
+  incCounter = () => {
     this.setState({ counter: this.state.counter + 1 });
   };
-
+  decCounter = () => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
   render() {
     const { old, car } = this.props;
     return (
       <div>
         <h1>Welcome to ReactJS from CC</h1>
-        <p>Age: {old}</p>
+        {/* <p>Age: {old}</p>
+        
         <p>Age: {car}</p>
-        <p>Student: {this.state.student}</p>
+        <p>Student: {this.state.student}</p> */}
         <p>Counter: {this.state.counter}</p>
-        <button onClick={this.changeCounter}>Increase</button>
+        <button onClick={this.incCounter}>Increase</button>
+        <button onClick={this.decCounter}>Decrement</button>
       </div>
     );
   }
